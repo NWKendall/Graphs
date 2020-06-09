90f2eb9b935ada85d54c449a1413eb0df55abde1
nicbongo <nicbongo@protonmail.com>
2020-06-09 11:39:58 -0300

    start

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,33 +13,62 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertext does not exist")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # queue
+        q = Queue()
+        q.enqueue(starting_vertex)
+
+        visited = set()
+        while q.size() > 0:
+            v = q.dequeue()
+
+            if v not in visited:
+                print(v)
+                visited.add(v)
+
+                for next_vert in self.get_neighbors(v):
+                    q.enqueue(next_vert)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # stack
+        st = Stack()
+        st.push(starting_vertex)
+
+        visited = set()
+        while st.size() > 0:
+            v = st.pop()
+
+            if v not in visited:
+                print(v)
+                visited.add(v)
+
+                for next_vert in self.get_neighbors(v):
+                    st.push(next_vert)
 
     def dft_recursive(self, starting_vertex):
         """
