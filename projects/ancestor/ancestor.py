d8fa5d64b9e87b8ced488fab43e0d8450ff7fa9b
nicbongo <nicbongo@protonmail.com>
2020-06-10 11:38:09 -0300

    Tuesday: MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,15 +1,19 @@
 class Queue():
     def __init__(self):
         self.queue = []
+
     def enqueue(self, value):
         self.queue.append(value)
+
     def dequeue(self):
         if self.size() > 0:
             return self.queue.pop(0)
         else:
             return None
+
     def size(self):
         return len(self.queue)
+
 
 class Graph:
 
@@ -28,16 +32,16 @@ class Graph:
     def get_neighbors(self, vertex_id):
         return self.vertices[vertex_id]
 
+
 def earliest_ancestor(ancestors, starting_node):
-    
+
     graph = Graph()
 
     for pair in ancestors:
         graph.add_vertex(pair[0])
         graph.add_vertex(pair[1])
-
+    for pair in ancestors:
         graph.add_edge(pair[1], pair[0])
-
 
     q = Queue()
     q.enqueue([starting_node])
@@ -52,11 +56,11 @@ def earliest_ancestor(ancestors, starting_node):
 
         if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
             print("EA2", earliest_ancestor)
-            max_path_len = len(path)
             earliest_ancestor = v
+            max_path_len = len(path)
             print("EA3", earliest_ancestor)
 
-        for neighbour in graph.get_neighbors(v):
+        for neighbour in graph.vertices[v]:
             new_path = list(path)
             new_path.append(neighbour)
             print("NP", new_path)
@@ -67,13 +71,6 @@ def earliest_ancestor(ancestors, starting_node):
     return earliest_ancestor
 
 
-
-
-
-
-
-
-
 # need node class?
     # value, left, right
 # search for starting node
@@ -81,6 +78,7 @@ def earliest_ancestor(ancestors, starting_node):
 # recieve [(paren, child)...], target
 # return int
 
-l = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+l = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+      (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
 print(earliest_ancestor(l, 6))
