7371985630112abc74905aa1376582b66295ea93
nicbongo <nicbongo@protonmail.com>
2020-06-14 22:24:12 -0300

    Sprint: MVPgit add .! Just

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -12,11 +12,11 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 
-# map_file = "maps/test_line.txt" ✅
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -32,27 +32,69 @@ player = Player(world.starting_room)
 traversal_path = []
 
 # dict to store rooms and their doors
-room_dict = dict()
+explored = dict()
 
 # initialize stack
-
 st = Stack()
-st.push(player.current_room)
+# st.push(player.current_room)
+# to convert directions to their opposites
+backtracking = {"n": "s", "e": "w", "s": "n", "w": "e"}
 
 # mapping continues until dictionary popualtion is equal to number of rooms / dynamic
-while st.size() > 0:
-    # if room doesnt exist
-    cur_room = st.pop()
-    # print("stack", st.size(), "st.pop", cur_room)
-    if player.current_room.id not in room_dict:
-        # add to dictionary, values being cardinal directions
-        room_dict[player.current_room.id] = player.current_room.get_exits()
-        for cardinal in room_dict[player.current_room.id]:
-            # st.push(player.current_room)
-            print("c", cardinal)
-            traversal_path.append(cardinal)
-            st.push(player.travel(cardinal))
-            print("here", player.current_room.id)
+while len(explored) < len(world.rooms):
+    # if room doesnt exist in dict
+    if player.current_room.id not in explored:
+        # add to dictionary, return values being cardinal directions
+        explored[player.current_room.id] = player.current_room.get_exits()
+        print(player.current_room.id, explored[player.current_room.id])
+
+        # for every connected room that hasn't been explored, go explore
+        if len(explored[player.current_room.id]) is not 0:
+            # explore the last room 
+            next_room = explored[player.current_room.id].pop()
+            # print("pop", next_room)
+            # save the cardinal for backtracking!!!!!
+            st.push(next_room)
+            # append direction to path
+            traversal_path.append(next_room)
+            # go to next room!
+            player.travel(next_room)
+        # if deadend or no more rooms to be explored
+        else:
+            # get last cardinal from prev room stored on stack
+            cardinal = st.pop()
+            # get opposite
+            prev_room = backtracking[cardinal]
+            # move!
+            player.travel(prev_room)
+            # append to path
+            traversal_path.append(prev_room)
+    # if has already been explored, do the same!!!!
+    else:
+        if len(explored[player.current_room.id]) is not 0:
+            # explore the last room 
+            next_room = explored[player.current_room.id].pop()
+            # print("pop", next_room)
+            # save the cardinal for backtracking!!!!!
+            st.push(next_room)
+            # append direction to path
+            traversal_path.append(next_room)
+            # go to next room!
+            player.travel(next_room)
+        # if deadend or no more rooms to be explored
+        else:
+            # get last cardinal from prev room stored on stack
+            cardinal = st.pop()
+            # get opposite
+            prev_room = backtracking[cardinal]
+            # move!
+            player.travel(prev_room)
+            # append to path
+            traversal_path.append(prev_room)
+
+print("TP:", traversal_path)
+print("EXPLORED:", explored)
+
 
 # ##### PLAYER AND ROOM METHOD DESCRIPTIONS #####
 # print("get_exits:", player.current_room.get_exits())
@@ -65,9 +107,6 @@ while st.size() > 0:
 # # return list of coords
 # print(player.travel("n"), player.current_room)
 # # updates player.current room
-
-# print("ROUTE:", traversal_path)
-# print("ROOM_DICT:", room_dict)
 
 # TRAVERSAL TEST
 
